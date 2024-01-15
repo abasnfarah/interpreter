ad95abc60df1234a4291b497d1cdffa1541da1f6
Abas Farah <abasnfarah@gmail.com>
2024-01-15 01:53:36 -0600

    Create token data structure

---
+++ go/monkey/token/token.go
@@ -0,0 +1,33 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+type tokenType string
+
+type Token struct {
+	Type    tokenType
+	Literal string
+}
