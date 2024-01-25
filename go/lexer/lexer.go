8dca72c82658709325c6e2834217ccaa06f299ff
Abas Farah <abasnfarah@gmail.com>
2024-01-25 17:31:13 -0600

    updates lexer

--- go/lexer/lexer.go
+++ go/lexer/lexer.go
@@ -1,6 +1,10 @@
 package lexer
 
-import "github.com/abasnfarah/interpreter/go/token"
+import (
+	"fmt"
+
+	"github.com/abasnfarah/interpreter/go/token"
+)
 
 type Lexer struct {
 	input        string
@@ -15,6 +19,10 @@ func New(input string) *Lexer {
 	return l
 }
 
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -25,8 +33,42 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	fmt.Printf("l.ch = %c\n", l.ch)
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -48,12 +90,22 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			fmt.Printf("tok = %v\n", tok)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
+
+	fmt.Printf("tok = %v\n", tok)
 
 	l.readChar()
 	return tok
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
 }
