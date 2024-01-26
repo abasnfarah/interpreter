c75ac147fc882a67f1eb7c0737557eead42dfa18
Abas Farah <abasnfarah@gmail.com>
2024-01-26 00:33:37 -0600

    Fixes lexer integer double read error

--- go/lexer/lexer_test.go
+++ go/lexer/lexer_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five = 5;;
-						let ten = 10;;
+	input := `let five = 5;
+						let ten = 10;
 
 						let add = fn(x, y) {
 							x + y;
