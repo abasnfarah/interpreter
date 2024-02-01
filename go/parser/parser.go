b9b1ff5a5806a4217f8147991ff5d68f15766959
Abas Farah <abasnfarah@gmail.com>
2024-01-31 18:44:39 -0600

    Initial parser (#13)
    * Initial parser
    
    * Updates parser import paths

---
+++ go/parser/parser.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"github.com/abasnfarah/monkey-interpreter/go/ast"
+	"github.com/abasnfarah/monkey-interpreter/go/lexer"
+	"github.com/abasnfarah/monkey-interpreter/go/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
